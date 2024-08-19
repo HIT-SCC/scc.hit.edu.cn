69f70f192b5c65ee130c7230368c58830aa25bda
Kun Ouyang <97422688+StarryNight-2022@users.noreply.github.com>
2024-08-19 12:46:28 +0800

    更新ASC24

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-make clean && make html && rmdir /s /q "./docs" && xcopy "./build/html" "./docs" /E && echo > docs/.nojekyll
+make clean && make html && rmdir /s /q "./docs" && xcopy "./build/html" "./docs" /E && echo > docs/.nojekyll && mkdir "./docs/_pdf" &&xcopy "./source/pdf" "./docs/_pdf" /E
